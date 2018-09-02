b7da10b3b4592bc665c2609b2522f379ea1b9aa6
Oliver Griffith-Jones <oliver@ncdevs.com>
2018-09-02 15:18:01 +1200

    Moved away from tab control.
    Was having issues due to the requirements of the internal well using tab control.
    
    Added error checking when adding a film checking for bad input types as well as duplicates.

--- filmStore.py
+++ filmStore.py
@@ -1,15 +1,6 @@
-# -----------------------------
-#         IMPORTED
-#         CONTENT
-# -----------------------------
-
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-
-# -----------------------------
-#          BACKEND 
-# -----------------------------
 
 class Film:
     
@@ -30,6 +21,7 @@ class Film:
         else:
             messagebox.askyesno("Error!", "You are unable to have a stock grater than 20 items. Please check the number you are trying to stockup by and try again.")
 
+
     def _sell_film(self, new_sale):
         if self._stock - new_sale >= 0:
             self._num_sold = self._num_sold + new_sale
@@ -39,7 +31,6 @@ class Film:
 
 def update_label():
     film_info.set("")
-    
     for p in films:
         film_info.set(film_info.get() + p._name + "  $" + str(p._price) + " Stock:" + str(p._stock) + " Number Sold:" + str(p._num_sold) + "\n")
 
@@ -50,20 +41,35 @@ def update_optionmenu():
         film_menu.children["menu"].add_command(label=p._name, command=lambda film=p._name: selected_film.set(film))
     selected_film.set("Select a film")
 
-def update_sell_menu():
-    global sell_film_menu
-    sell_film_menu.children["menu"].delete(0, "end")
-    for f in films:
-        sell_film_menu.children["menu"].add_command(label=f._name, command=lambda film=f._name: selected_film.set(film))
-    selected_film.set("Select a film")
-
 # a new film, update the price and update the stock
 def add():
     global film_menu
-    Film(new_film.get(), new_price.get(), 0, 0)
+    # Check if any films exsits with the same name. If so tell the user that film allready exists.
+    if not any(f._name == new_film.get() for f in films):
+        # Try change the input value to an int
+        try:
+            add_price = int(new_price.get())
+
+            # Check if the entered price is larger than 0, if it is zero then ask the user that they ment to set the price to 0
+            if add_price >= 0 and new_film.get():
+                if new_price.get() == 0:
+                    if messagebox.askyesno("Film price is $0", "Is this correct?"):
+                        Film(new_film.get(), new_price.get(), 0, 0)
+                else:
+                    # Confim wether the user wants to add the film.
+                    check_messege = "Are you sure you want to add the film: " + new_film.get() + " With a price of: $" + new_price.get()
+                    if messagebox.askyesno("Check New Film", check_messege):
+                        Film(new_film.get(), new_price.get(), 0, 0)
+            else:
+                messagebox.showinfo("Error", "The films price must be a positive interger e.g. 0 and you must set a film name")
+        # If changing the value to an Int errors then ask the user to enter a postive interger
+        except ValueError:
+            messagebox.showinfo("Error", "The films price must be a positive interger e.g. 0")
+    else:
+        check_messege = "The film " + new_film.get() + " Already exists."
+        messagebox.showinfo("Duplicate Films", check_messege)
     update_label()
     update_optionmenu()
-    update_sell_menu()
 
 def edit():
     for f in films:
@@ -83,6 +89,7 @@ def update_stock():
 def sell_film():
     for f in films:
         if f._name == selected_film.get():
+            f._update_stock(sale_num.get() * -1)
             f._sell_film(sale_num.get())
     update_label()
 
@@ -95,20 +102,6 @@ def delete():
         update_optionmenu()      
         update_label()
 
-# Show film info when clicked on
-def popup():
-    top = Toplevel()
-    top.title("Popup!")
-    name_val = StringVar()
-    name_val.set("HELLOOOOO")
-    lbl = Label(top, text="asdasdas").grid(row=0)
-    for p in films:
-        if p._name == selected_film.get():
-            name_val.set(p._name)
-            print(p._name)
-    name_lbl = Label(top, textvariable=name_val, bg="red").grid(row=1)
-    destroy_btn = Button(top, text="Close", command=top.destroy).grid(row=2)
-
 # films contains all objects
 # film_names contains the names of each film
 films = []
@@ -117,78 +110,45 @@ film_names = []
 Film("Iron Man", 10, 10, 0)
 Film("Ant Man", 5, 10, 0)
 
-# -----------------------------
-#           VIEWS 
-# -----------------------------
-
-# Create the window and give it a title. Set window size to fit the display.
-window = Tk()
-window.title("RentFlix")
-window.geometry('6000x5000')
- 
-#NoteBook Tabs / Frame Views
-tab_control = ttk.Notebook(window)
-# Create Frames
-admin = ttk.Frame(tab_control)
-sell = ttk.Frame(tab_control)
-add_film = ttk.Frame(tab_control)
- 
-# Page Controls & Give labels
-tab_control.add(admin, text='Admin')
-tab_control.add(sell, text='Sell')
-tab_control.add(add_film, text='Add Film')
- 
-
-
-# ----------- ADMIN -----------
+root = Tk()
+root.title("Add film")
+root.geometry('400x400')
 
 # label to display the price list
 film_info = StringVar()
 
-film_lbl = Label(admin, textvariable=film_info)
+film_lbl = Label(root, textvariable=film_info)
 film_lbl.grid(row = 0)
 
 # option menu with all films
 selected_film = StringVar()
 selected_film.set("Select a film")
-film_menu = OptionMenu(admin, selected_film, *film_names)
+film_menu = OptionMenu(root, selected_film, *film_names)
 film_menu.grid(row = 4)
-
-# entry field for editing stock
-edit_stock = IntVar()
-edit_entry = Entry(admin, textvariable=edit_stock).grid(row=7, column=1)
-
-# button to edit the stock
-edit_stock_btn = Button(admin, text="Edit Stock", command=update_stock).grid(row=7, column=2)
 
 # entry field for editing price
 edit_price = IntVar()
-edit_entry = Entry(admin, textvariable=edit_price).grid(row=4, column=1)
+edit_entry = Entry(root, textvariable=edit_price).grid(row=4, column=1)
 
 # button to edit the price
-edit_btn = Button(admin, text="Edit price", command=edit).grid(row=4, column=2)
+edit_btn = Button(root, text="Edit price", command=edit).grid(row=4, column=2)
 
-# delete button
-delete_btn = Button(admin, text="Delete", command=delete).grid(row=5, column=1)
+# entry field for editing stock
+edit_stock = IntVar()
+edit_entry = Entry(root, textvariable=edit_stock).grid(row=7, column=1)
 
-
-# ----------- Sell -----------
- 
-# Show Select Menu
-selected_film = StringVar()
-selected_film.set("Select a film")
-sell_film_menu = OptionMenu(sell, selected_film, *film_names)
-sell_film_menu.grid(row = 4)
+# button to edit the stock
+edit_btn = Button(root, text="Edit Stock", command=update_stock).grid(row=7, column=2)
 
 # Number To Sell
 sale_num = IntVar()
-edit_entry = Entry(sell, textvariable=sale_num).grid(row=8, column=1)
+edit_entry = Entry(root, textvariable=sale_num).grid(row=8, column=1)
 
 # button to sell
-edit_btn = Button(sell, text="Sell Film", command=sell_film).grid(row=8, column=2)
+edit_btn = Button(root, text="Sell Film", command=sell_film).grid(row=8, column=2)
 
-
-# ----------- Add Film -----------
+# delete button
+delete_btn = Button(root, text="Delete", command=delete).grid(row=5, column=1)
 
 # entry fields for new film name and price
 new_film = StringVar()
@@ -196,20 +156,11 @@ new_price = StringVar()
 new_film.set("Enter film name")
 new_price.set("Price")
 
-film_entry = Entry(add_film, textvariable = new_film).grid(row = 2)
-price_entry = Entry(add_film, textvariable = new_price).grid(row = 2, column = 1)
+film_entry = Entry(root, textvariable = new_film).grid(row = 2)
+price_entry = Entry(root, textvariable = new_price).grid(row = 2, column = 1)
 
 # button to add a new film  object
-add_btn = Button(add_film, text="Add new film", command=add).grid(row = 3)
+add_btn = Button(root, text="Add new film", command=add).grid(row = 3)
 
-# View / Tab Control
-tab_control.pack(expand=1, fill='both')
-
-
-# ----------- Page Controls -----------
-
-#Update Things
 update_label()
-
-# Inetiate the window
-window.mainloop()
+root.mainloop()
